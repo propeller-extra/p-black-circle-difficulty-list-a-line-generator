031acaac021aea3e93c287a3fa62baae1cf111c2
propeller-extra <node.js@zoho.com>
2020-06-30 14:13:19 +0900

    commit

--- src/app/models/p-circle-difficulty-list.ts
+++
@@ -1,7 +0,0 @@
-import { PCircleDifficultyAttributeList } from './p-circle-difficulty-attribute-list';
-
-export interface PCircleDifficultyList extends PCircleDifficultyAttributeList {
-  lv: number;
-  name: string;
-  link: string;
-}
