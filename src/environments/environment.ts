c7330f9300a6a013de7b22d4943947f5b1e08b29
propeller-extra <node.js@zoho.com>
2020-07-12 17:36:36 +0900

    create circleci config.yml

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAGJEEWkVGOVhg4e-Pd9YAHDlVGa_9Fzek',
+    authDomain: 'difficulty-list-line-generator.firebaseapp.com',
+    databaseURL: 'https://difficulty-list-line-generator.firebaseio.com',
+    projectId: 'difficulty-list-line-generator',
+    storageBucket: 'difficulty-list-line-generator.appspot.com',
+    messagingSenderId: '733409245561',
+    appId: '1:733409245561:web:c4fa41249d101b677f97a8',
+    measurementId: 'G-3C3DQVS3PZ',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
